ea45a52bb5d1a107c0aaedcc62f5d455f19554ba
MUTEMWA MBEWE <mutemwambewe@gmail.com>
2025-11-24 11:36:12 +0000

    on the mobile view ive noticed the information is a bit too far to the l

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -106,7 +106,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
         <Header 
           pageTitle={pageTitle}
         />
-        <main className="flex-1 overflow-y-auto p-4 sm:p-6">{children}</main>
+        <main className="flex-1 overflow-y-auto p-4 sm:px-6 sm:py-6">{children}</main>
       </div>
     </div>
   );
