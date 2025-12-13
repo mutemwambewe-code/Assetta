4873cf8a4a741f71d19fc5bd3dc350bbaa28c88d
MUTEMWA MBEWE <mutemwambewe@gmail.com>
2025-12-13 09:19:15 +0000

    i would like to update to the latest version of react and next js withou

--- next.config.js
+++ next.config.js
@@ -10,29 +10,7 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_AFRICASTALKING_USERNAME: process.env.AFRICASTALKING_USERNAME,
     NEXT_PUBLIC_AFRICASTALKING_SENDER_ID: process.env.AFRICASTALKING_SENDER_ID,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  }
 };
 
 module.exports = nextConfig;
