4a095a78646712fd4f5ef11cae75e3bb7f9d2060
MUTEMWA MBEWE <mutemwambewe@gmail.com>
2026-01-07 14:09:55 +0000

    I want this to be that icon that people see when they save the webapp on

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { ClientProviders } from '@/components/providers/client-providers';
+import { AppLogo } from '@/components/layout/app-logo';
 
 export const metadata: Metadata = {
   title: 'Assetta',
   description: 'Where your assets thrive, because we make them better.',
-  manifest: '/manifest.json'
 };
 
 export default function RootLayout({
@@ -19,8 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-        <meta name="theme-color" content="#1c7ed6" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <meta name="theme-color" content="#183D7A" />
       </head>
       <body className="font-body antialiased">
         <ClientProviders>{children}</ClientProviders>
