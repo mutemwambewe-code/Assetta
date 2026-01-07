0638c707f7cc38fe7e370dfcfa0d120c3c474d91
MUTEMWA MBEWE <mutemwambewe@gmail.com>
2026-01-07 13:21:13 +0000

    can you make this web app downloadable?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#1c7ed6" />
       </head>
       <body className="font-body antialiased">
         <ClientProviders>{children}</ClientProviders>
