561c6d0e8a987e8ce5994a92274ff0b7e9fc4ed6
MUTEMWA MBEWE <mutemwambewe@gmail.com>
2026-01-13 10:59:48 +0000

    let this be the favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><path fill=%22%23F5832E%22 d=%22M62.3,77.5l-8.6-20.3h-8.2l-8.4,20.3h-7.3L50,22.5l20.1,55H62.3z M50,33.1L43.1,51h13.8L50,33.1z%22/></svg>" />
         <meta name="theme-color" content="#183D7A" />
       </head>
       <body className="font-body antialiased">
